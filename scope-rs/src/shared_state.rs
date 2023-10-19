1e5bda24467f6838ea0eff1ee1077b455612d194
Carl <97188867+g-01234@users.noreply.github.com>
2023-10-18 18:38:56 -0700

    update

--- scope-rs/src/shared_state.rs
+++ scope-rs/src/shared_state.rs
@@ -54,7 +54,6 @@ impl SharedState {
     pub fn new() -> Self {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
         Self {
-            // endpoint: RwLock::new("https://eth-mainnet.g.alchemy.com/v2/L7I4VYVjOHg6fK8DlyevikVH8DKC2J7D".to_string()),
             endpoint: RwLock::new("http://127.0.0.1:8545".to_string()),
             has_focus: RwLock::new(true),
             ..Default::default()
